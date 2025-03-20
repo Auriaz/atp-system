8c9d71f742d074625b9fd94b5de7ed327f73cbcc
WebSpaceLab <133574117+WebSpaceLab@users.noreply.github.com>
2025-03-20 10:46:07 +0100

    fix: Looking for a bug for the login system

--- app/components/X/Form/Auth/Login/XFormAuthLogin.vue
+++ app/components/X/Form/Auth/Login/XFormAuthLogin.vue
@@ -16,62 +16,65 @@ const previewPassword = ref(false)
 
 const toast = useToast()
 async function onSubmit(event: FormSubmitEvent<loginSchema>) {
-  await $fetch('/api/auth/login', {
-    method: 'POST',
-    body: event.data
+  const {data, error} = await useAsyncData('login' , () => {
+    return $fetch('/api/auth/login', {
+      method: 'POST',
+      body: state
+    })
   })
-  .then((res) => {
-    fetch()
 
-    toast.add(res.message as Toast)
-    navigateTo('/dashboard')
-  })
-  .catch((error) => {
+  if(error.value) {
     toast.add({
       title: 'Error',
-      description: error.message,
+      description: error.value.data?.message as string,
       color: 'error'
     })
+    return
+  }
 
-    console.error(error)
-  })
+  fetch()
+
+  toast.add(data.value?.message as Toast)
+  navigateTo('/dashboard')
 }
 </script>
 
 <template>
-  <UForm :schema="v.safeParser(loginSchema)" :state="state" class="w-full space-y-6 px-6" @submit="onSubmit">
-    <UFormField required label="Email" name="email" class="w-full">
-      <UInput v-model="state.email" class="w-full"/>
-    </UFormField>
-
-    <UFormField required class="w-full relative" label="Password" name="password">
-      <UInput v-model="state.password" :type="previewPassword ? 'text' : 'password'" class="w-full" >
-        <template #trailing>
-          <UButton
-            @click="previewPassword = !previewPassword"
-            color="secondary"
-            size="sm"
-            variant="link"
-            :icon="previewPassword ? 'i-lucide-eye-off' : 'i-lucide-eye'"
-            :aria-label="previewPassword ? 'Hide password' : 'Show password'"
-            :aria-pressed="previewPassword"
-            aria-controls="password"
-          />
-        </template>
-      </UInput>
-    </UFormField>
-
-    <div class="flex items-center space-x-2">
-      <UCheckbox v-model="state.rememberMe"  color="primary" />
-      <span class="text-sm">Remember me</span>
-    </div>
-
-    <UButton type="submit" color="primary" variant="solid" block class="text-bold">
-      Submit
-    </UButton>
-
+  <div class="w-full space-y-6 px-6">
+    <UForm :schema="v.safeParser(loginSchema)" :state="state" class="w-full space-y-6" @submit="onSubmit">
+      <UFormField required label="Email" name="email" class="w-full">
+        <UInput v-model="state.email" class="w-full"/>
+      </UFormField>
+  
+      <UFormField required class="w-full relative" label="Password" name="password">
+        <UInput v-model="state.password" :type="previewPassword ? 'text' : 'password'" class="w-full" >
+          <template #trailing>
+            <UButton
+              @click="previewPassword = !previewPassword"
+              color="secondary"
+              size="sm"
+              variant="link"
+              :icon="previewPassword ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+              :aria-label="previewPassword ? 'Hide password' : 'Show password'"
+              :aria-pressed="previewPassword"
+              aria-controls="password"
+            />
+          </template>
+        </UInput>
+      </UFormField>
+  
+      <div class="flex items-center space-x-2">
+        <UCheckbox v-model="state.rememberMe"  color="primary" />
+        <span class="text-sm">Remember me</span>
+      </div>
+  
+      <UButton type="submit" color="primary" variant="solid" block class="text-bold">
+        Submit
+      </UButton>
+    </UForm>
+  
     <USeparator />
-
+  
     <XBtnOAuthLogin />
-  </UForm>
+  </div>
 </template>
