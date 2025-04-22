db6eb6167df1565a77f334cb929c2072328a794d
WebSpaceLab <133574117+WebSpaceLab@users.noreply.github.com>
2025-04-22 22:19:42 +0200

    feat: enable '@vueuse/nuxt' module in nuxt.config.ts and add new iconify-json dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
     'nuxt-auth-utils',
     // '@pinia/nuxt',
     // 'pinia-plugin-persistedstate/nuxt',
-    // '@vueuse/nuxt',
+    '@vueuse/nuxt',
     // '@nuxtjs/i18n'
   ],
 
